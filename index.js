a045e6716a87510173c58d0e633a702fe2403220
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-02-07 15:57:47 -0800

    Tracekit sub.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import TraceKit from './bower_components/tracekit';
+import winston from 'winston';
 
 const app = express()
 
 app.get('/', function(req, res){
 	res.send('Hello There!')
 })
+
+function onError(window){
+	console.log("something", window);
+}
+
+TraceKit.report.subscribe(onError());
+
+
 
 const port = process.env.PORT || 3000
 
